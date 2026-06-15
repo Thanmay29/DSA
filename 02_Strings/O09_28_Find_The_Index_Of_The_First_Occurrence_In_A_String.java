d272c4403ddf76f81e97ee164accbb3121541e2e
Thanmay Patel V <143529683+Thanmay29@users.noreply.github.com>
2026-06-15 12:50:25 +0530

    Update O09_28_Find_The_Index_Of_The_First_Occurrence_In_A_String.java

--- 02_Strings/O09_28_Find_The_Index_Of_The_First_Occurrence_In_A_String.java
+++ 02_Strings/O09_28_Find_The_Index_Of_The_First_Occurrence_In_A_String.java
@@ -9,21 +9,64 @@ public class O09_28_Find_The_Index_Of_The_First_Occurrence_In_A_String {
 
 //best solution
 
-public static int strStr(String haystack, String needle) {
-    int m = haystack.length(), n = needle.length();
+class Solution {
 
-    for (int i = 0; i <= m - n; i++) {
-        int j = 0;
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
 
-        while (j < n && haystack.charAt(i + j) == needle.charAt(j)) {
-            j++;
+        int[] lps = buildLPS(needle);
+
+        int i = 0; // haystack pointer
+        int j = 0; // needle pointer
+
+        while (i < haystack.length()) {
+
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == needle.length()) {
+                    return i - j; // match found
+                }
+
+            } else {
+                if (j > 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
         }
 
-        if (j == n) return i;
+        return -1;
     }
 
-    return -1;
+    // Build LPS array
+    private int[] buildLPS(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int len = 0;
+        int i = 1;
+
+        while (i < pattern.length()) {
+
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
+    }
 }
+
 
     public static int strStr(String haystack, String needle) {
         int m =haystack.length(), n = needle.length();
