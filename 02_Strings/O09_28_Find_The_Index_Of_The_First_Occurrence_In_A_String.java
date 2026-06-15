1692cfe2721b2b8af1ed2b462d03a39902cb0579
Thanmay Patel V <143529683+Thanmay29@users.noreply.github.com>
2026-06-15 12:48:14 +0530

    Update O09_28_Find_The_Index_Of_The_First_Occurrence_In_A_String.java

--- 02_Strings/O09_28_Find_The_Index_Of_The_First_Occurrence_In_A_String.java
+++ 02_Strings/O09_28_Find_The_Index_Of_The_First_Occurrence_In_A_String.java
@@ -7,6 +7,24 @@ public class O09_28_Find_The_Index_Of_The_First_Occurrence_In_A_String {
         System.out.println(strStr(haystack, needle));
     }
 
+//best solution
+
+public static int strStr(String haystack, String needle) {
+    int m = haystack.length(), n = needle.length();
+
+    for (int i = 0; i <= m - n; i++) {
+        int j = 0;
+
+        while (j < n && haystack.charAt(i + j) == needle.charAt(j)) {
+            j++;
+        }
+
+        if (j == n) return i;
+    }
+
+    return -1;
+}
+
     public static int strStr(String haystack, String needle) {
         int m =haystack.length(), n = needle.length();
 
