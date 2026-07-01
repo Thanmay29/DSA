cbacfeef59d9c5767a6a08c0c8747471649295dc
Thanmay Patel V <143529683+Thanmay29@users.noreply.github.com>
2026-07-01 15:41:38 +0530

    Update O01_496_Next_Greater_Element_I.java

--- 06_Stack/Monotonic_Stack/O01_496_Next_Greater_Element_I.java
+++ 06_Stack/Monotonic_Stack/O01_496_Next_Greater_Element_I.java
@@ -7,6 +7,29 @@ import java.util.Stack;
 
 public class O01_496_Next_Greater_Element_I {
 
+    //Easier sol
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = nums2.length-1; i>=0; i--){
+            while(!st.isEmpty() && nums2[i] >= st.peek()){
+                st.pop();
+            }
+
+            map.put(nums2[i], st.isEmpty() ? -1 : st.peek());
+            st.push(nums2[i]);
+        }
+
+        int[] res = new int[nums1.length];
+        for(int i = 0; i<nums1.length; i++){
+            res[i] = map.get(nums1[i]);
+        }
+
+        return res;
+    }
+
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
         // Map to store the next greater element for each number in nums2
         Map<Integer, Integer> nextGreaterMap = new HashMap<>();
